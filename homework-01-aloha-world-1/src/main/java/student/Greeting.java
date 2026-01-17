7d3579f6360c116b158625959a58f0753794428e
arshsinghphd <57355255+arshsinghphd@users.noreply.github.com>
2026-01-16 21:16:51 -0800

    syntax correction

--- homework-01-aloha-world-1/src/main/java/student/Greeting.java
+++ homework-01-aloha-world-1/src/main/java/student/Greeting.java
@@ -1,7 +1,7 @@
 package student;
 
 /*
- * This class is a place holder which you will fully implement based on the javadoc
+ * This class is a placeholder which you will fully implement based on the javadoc
  * 
  * https://cs5004-khoury-lionelle.github.io/hello_world/student/package-summary.html
  * 
